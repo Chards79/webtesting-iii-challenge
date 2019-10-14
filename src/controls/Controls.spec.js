e43bb90f0852b2b99efea7f4165fc2ea2e425a07
Eric Richards <esholundr@yahoo.com>
2019-10-14 17:13:40 -0600

    Installed dependencies

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+
+import Controls from './Controls';
+
+test('it renders correctly', () => {
+    render(<Controls />);
+});
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react'
+import { render } from 'react-testing-library';
+
+import Display from './Display';
+
+test('Mocking example', () => {
+    const mock = jest.fn();
+    mock('smile');
+    expect(mock).toHaveBeenCalled();
+})
