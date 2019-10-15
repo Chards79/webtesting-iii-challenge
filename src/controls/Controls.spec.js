b22dd33e3e11717e577295a2002b9c848c273be2
Eric Richards <esholundr@yahoo.com>
2019-10-15 10:04:56 -0600

    Control tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -8,3 +8,18 @@ test('it renders correctly', () => {
     render(<Controls />);
 });
 
+test('button text changes upon toggle', () => {
+    const toggleLockedMock = jest.fn();
+    const { getByText } = render(
+        <Controls toggleLocked={toggleLockedMock} />
+    );
+    getByText(/lock gate/i);
+});
+
+test('button text changes upon closed', () => {
+    const toggleClosedMock = jest.fn();
+    const { getByText } = render(
+        <Controls toggleClosed={toggleClosedMock} />
+    );
+    getByText(/close gate/i);
+});
